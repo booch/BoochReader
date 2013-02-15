55d69ffdd6e078f8b33117a31db01729e737ee5a
Craig Buchek <craig@boochtek.com>
2013-02-14 22:19:32 -0600

    Upgrade to PhoneGap 2.4.
    Updated JS for WebOS app menu to use new event name.
    Fix .gitignore so it doesn't ignore some PhoneGap files.

--- app/assets/javascripts/webos.js
+++ app/assets/javascripts/webos.js
@@ -1,11 +1,16 @@
 document.addEventListener("deviceready", function(){
-    // PhoneGap docs say the platform name is 'webOS', but we're actually getting 'palm'.
-    if ( device.platform == 'webOS' || device.platform == 'palm' ) {
+    // PhoneGap docs say the platform name is 'webOS', but we were actually getting 'palm' in PhoneGap 2.0, and 'HP webOS' in 2.3.
+    if ( device.platform == 'HP webOS' || device.platform == 'webOS' || device.platform == 'palm' ) {
         // Only allow landscape orientation.
         navigator.orientation.setOrientation('right');
         
-        // Handle the WebOS App Menu. Note that PhoneGap calls this event 'appmenuopen', while Enyo calls it 'openAppMenu'.
+        // Handle the WebOS App Menu. Enyo calls it 'openAppMenu'. NOTE: PhoneGap 2.2 and 2.3 don't seem to support this.
+        // NOTE: PhoneGap 2.0 and 2.1 call this event 'appmenuopen'.
         window.addEventListener('appmenuopen', function() {
+            $('#webos-app-menu').show();
+        });
+        // NOTE: PhoneGap 2.4 calls this 'menubutton' and moves it to document -- which makes it match Android hardware menu button.
+        document.addEventListener('menubutton', function() {
             $('#webos-app-menu').show();
         });
         // Close the menu when an item is clicked.
